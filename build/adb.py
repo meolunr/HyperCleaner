47d57a8f0cac7c9f9cd6d9baf6c54b3f9fb6a1b9
meolunr <meolunr@gmail.com>
2021-12-31 00:11:59 +0800

    Pull and push files from the phone

--- build/adb.py
+++ build/adb.py
@@ -12,3 +12,22 @@ class AdbUtils(object):
     @staticmethod
     def exec_as_root(command: str):
         os.system('adb shell su -c %s' % command)
+
+    @staticmethod
+    def pull(command: str):
+        os.system('adb pull %s > nul' % command)
+
+    @staticmethod
+    def push(command: str):
+        os.system('adb push %s' % command)
+
+    @staticmethod
+    def push_as_root(command: str):
+        split = command.split(' ')
+        src = split[0]
+        dst = split[1]
+        file = os.path.basename(src)
+
+        AdbUtils.push('%s %s' % (src, DATA_TMP_PATH))
+        AdbUtils.exec_as_root('cp -rf %s %s' % (os.path.join(DATA_TMP_PATH, file), dst))
+        AdbUtils.exec('rm -rf %s' % os.path.join(DATA_TMP_PATH, file))
