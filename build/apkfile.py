5df4664e1302487813500a1f04c4d486ecdaf807
meolunr <meolunr@gmail.com>
2021-12-31 21:23:25 +0800

    Use absolute paths to decode

--- build/apkfile.py
+++ build/apkfile.py
@@ -6,15 +6,15 @@ from build.apktool import ApkTool
 class ApkFile(object):
 
     def __init__(self, file: str):
-        self.name = file
-        self.output_dir = None
+        self.__path = os.path.abspath(file)
+        self.__output_dir = None
 
     def decode(self, need_res=False):
-        self.output_dir = os.path.splitext(self.name)[0]
-        ApkTool.decode(self.name, self.output_dir, need_res)
+        self.__output_dir = os.path.splitext(self.__path)[0]
+        ApkTool.decode(self.__path, self.__output_dir, need_res)
 
     def build(self):
-        output_file = os.path.join(self.output_dir, 'dist', self.name)
-        ApkTool.build(self.output_dir)
+        output_file = os.path.join(self.__output_dir, 'dist', os.path.basename(self.__path))
+        ApkTool.build(self.__output_dir)
         ApkTool.sign(output_file)
         return output_file
