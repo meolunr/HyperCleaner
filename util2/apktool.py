1face5d2960da2b7d3a2b31338d203afd0d12d6c
meolunr <meolunr@gmail.com>
2024-05-09 15:44:21 +0800

    Update zipalign version to 34.0.0

--- util2/apktool.py
+++ util2/apktool.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 LIB_DIR = os.path.join(sys.path[0], 'lib')
+BIN_DIR = os.path.join(sys.path[0], 'bin')
 
 
 def decode(file: str, output: str, no_res=True):
@@ -12,3 +13,10 @@ def decode(file: str, output: str, no_res=True):
 def build(dir_path: str, output: str, copy_original=True):
     original_param = '-c' if copy_original else ''
     print(f'java -jar {LIB_DIR}/apktool.jar b {original_param} {dir_path} -o {output}')
+
+
+def zipalign(file: str):
+    old_file = f'{file}.old'
+    os.rename(file, old_file)
+    print(f'{BIN_DIR}/zipalign.exe -p 4 {old_file} {file}')
+    os.remove(old_file)
