75829c8cf24543457f65c691e32ded74b247737e
meolunr <meolunr@gmail.com>
2024-05-09 14:36:19 +0800

    Move temporarily unused code

--- customizer.py
+++ customizer.py
@@ -1,10 +1,12 @@
 import os
+import shutil
 
+from build import ApkFile
+from build.method_specifier import MethodSpecifier
 from util import AdbUtils
 
 
 def rm_files():
-    # TODO: Placeholder
     def ignore_annotation(line: str):
         annotation_index = line.find('#')
         if annotation_index >= 0:
@@ -23,6 +25,142 @@ def rm_files():
             if len(rubbish) != 0:
                 print('Deleting %s' % rubbish)
                 AdbUtils.exec_as_root('rm -rf %s' % rubbish)
+
+
+def process_in_tmp(func):
+    def wrapper(*args, **kwargs):
+        cwd = os.getcwd()
+        if os.path.exists('tmp'):
+            print('Delete temp files ...')
+            shutil.rmtree('tmp')
+
+        os.mkdir('tmp')
+        os.chdir('tmp')
+        result = func(*args, **kwargs)
+
+        os.chdir(cwd)
+        shutil.rmtree('tmp')
+        return result
+
+    return wrapper
+
+
+def disable_wakeup_dialog(apk_file: ApkFile):
+    smali_file = apk_file.open_smali('com/miui/wakepath/ui/ConfirmStartActivity.smali')
+    specifier = MethodSpecifier()
+    specifier.access = MethodSpecifier.Access.PROTECTED
+    specifier.keywords.append('"android.intent.action.PICK"')
+    new_method_fragment = '''\
+    const/4 v0, 0x0
+
+    const/4 v1, -0x1
+
+    invoke-virtual {p0, v0, v1}, Lcom/miui/wakepath/ui/ConfirmStartActivity;->onClick(Landroid/content/DialogInterface;I)V
+
+    invoke-virtual {p0}, Landroid/app/Activity;->finish()V
+
+    return-void\
+    '''
+    new_method_body = smali_file.find_method(specifier).replace('return-void', new_method_fragment)
+    smali_file.method_replace(smali_file.find_method(specifier), new_method_body)
+
+
+def disable_wifi_blocked_notification(apk_file: ApkFile):
+    smali_file = apk_file.open_smali('com/miui/networkassistant/utils/NotificationUtil.smali')
+    specifier = MethodSpecifier()
+    specifier.name = 'sendWifiNetworkBlockedNotify'
+    smali_file.method_nop(specifier)
+
+
+def lock_100_score(apk_file: ApkFile):
+    specifier = MethodSpecifier()
+
+    smali_file = apk_file.open_smali('com/miui/securityscan/ui/main/MainContentFrame.smali')
+    specifier.name = 'onClick'
+    smali_file.method_nop(specifier)
+
+    smali_file = apk_file.open_smali('com/miui/securityscan/scanner/ScoreManager.smali')
+    specifier.name = None
+    specifier.keywords.append('getMinusPredictScore')
+    smali_file.method_return0(specifier)
+
+
+def disable_cloud_control(apk_file: ApkFile):
+    smali_file = apk_file.open_smali('com/miui/powerkeeper/cloudcontrol/LocalUpdateUtils.smali')
+    specifier = MethodSpecifier()
+    specifier.name = 'startCloudSyncData'
+    smali_file.method_nop(specifier)
+
+
+def global_maximum_fps(apk_file: ApkFile):
+    smali_file = apk_file.open_smali('com/miui/powerkeeper/statemachine/DisplayFrameSetting.smali')
+    specifier = MethodSpecifier()
+    specifier.name = 'setScreenEffect'
+    specifier.parameters = 'Ljava/lang/String;II'
+    smali_file.method_nop(specifier)
+
+
+@process_in_tmp
+def process_security_center():
+    print('>>> Process SecurityCenter.apk')
+    AdbUtils.pull('/system/priv-app/SecurityCenter/SecurityCenter.apk')
+    apk_file = ApkFile('SecurityCenter.apk')
+    apk_file.decode()
+
+    disable_wakeup_dialog(apk_file)
+    disable_wifi_blocked_notification(apk_file)
+    lock_100_score(apk_file)
+
+    path = apk_file.build()
+    AdbUtils.push_as_root(path, '/system/priv-app/SecurityCenter/')
+
+
+@process_in_tmp
+def process_power_keeper():
+    print('>>> Process PowerKeeper.apk')
+    AdbUtils.pull('/system/app/PowerKeeper/PowerKeeper.apk')
+    apk_file = ApkFile('PowerKeeper.apk')
+    apk_file.decode()
+
+    disable_cloud_control(apk_file)
+    global_maximum_fps(apk_file)
+
+    path = apk_file.build()
+    AdbUtils.push_as_root(path, '/system/app/PowerKeeper/')
+    AdbUtils.exec_as_root('rm -rf /data/vendor/thermal/config')
+
+
+@process_in_tmp
+def process_joyose():
+    print('>>> Process Joyose.apk')
+    AdbUtils.pull('/system/app/Joyose/Joyose.apk')
+    apk_file = ApkFile('Joyose.apk')
+    apk_file.decode()
+
+    smali_file = apk_file.find_smali('allow connect:')
+    specifier = MethodSpecifier()
+    specifier.keywords.append('allow connect:')
+    smali_file.method_nop(specifier)
+
+    path = apk_file.build()
+    AdbUtils.push_as_root(path, '/system/app/Joyose/')
+    AdbUtils.exec_as_root('pm clear com.xiaomi.joyose')
+
+
+@process_in_tmp
+def process_systemui():
+    print('>>> Process MiuiSystemUI.apk')
+    AdbUtils.pull('/system_ext/priv-app/MiuiSystemUI/MiuiSystemUI.apk')
+    apk_file = ApkFile('MiuiSystemUI.apk')
+    apk_file.decode()
+
+    smali_file = apk_file.open_smali('com/android/settingslib/bluetooth/LocalBluetoothAdapter.smali')
+    specifier = MethodSpecifier()
+    specifier.name = 'isSupportBluetoothRestrict'
+    smali_file.method_return0(specifier)
+
+    path = apk_file.build()
+    AdbUtils.push_as_root(path, '/system_ext/priv-app/MiuiSystemUI/')
 
 
 def run():
--- main.py
+++ main.py
@@ -11,9 +11,6 @@ import config
 import customizer
 import imgfile
 import vbmeta
-from build import ApkFile
-from build.method_specifier import MethodSpecifier
-from util import AdbUtils
 
 BIN_DIR = os.path.join(sys.path[0], 'bin')
 OVERLAY_DIR = os.path.join(sys.path[0], 'overlay')
@@ -22,142 +19,6 @@ OVERLAY_DIR = os.path.join(sys.path[0], 'overlay')
 def log(string: str):
     now = datetime.now().strftime('[%m-%d %H:%M:%S]')
     print(f'{now} {string}')
-
-
-def process_in_tmp(func):
-    def wrapper(*args, **kwargs):
-        cwd = os.getcwd()
-        if os.path.exists('tmp'):
-            print('Delete temp files ...')
-            shutil.rmtree('tmp')
-
-        os.mkdir('tmp')
-        os.chdir('tmp')
-        result = func(*args, **kwargs)
-
-        os.chdir(cwd)
-        shutil.rmtree('tmp')
-        return result
-
-    return wrapper
-
-
-def disable_wakeup_dialog(apk_file: ApkFile):
-    smali_file = apk_file.open_smali('com/miui/wakepath/ui/ConfirmStartActivity.smali')
-    specifier = MethodSpecifier()
-    specifier.access = MethodSpecifier.Access.PROTECTED
-    specifier.keywords.append('"android.intent.action.PICK"')
-    new_method_fragment = '''\
-    const/4 v0, 0x0
-
-    const/4 v1, -0x1
-
-    invoke-virtual {p0, v0, v1}, Lcom/miui/wakepath/ui/ConfirmStartActivity;->onClick(Landroid/content/DialogInterface;I)V
-
-    invoke-virtual {p0}, Landroid/app/Activity;->finish()V
-
-    return-void\
-    '''
-    new_method_body = smali_file.find_method(specifier).replace('return-void', new_method_fragment)
-    smali_file.method_replace(smali_file.find_method(specifier), new_method_body)
-
-
-def disable_wifi_blocked_notification(apk_file: ApkFile):
-    smali_file = apk_file.open_smali('com/miui/networkassistant/utils/NotificationUtil.smali')
-    specifier = MethodSpecifier()
-    specifier.name = 'sendWifiNetworkBlockedNotify'
-    smali_file.method_nop(specifier)
-
-
-def lock_100_score(apk_file: ApkFile):
-    specifier = MethodSpecifier()
-
-    smali_file = apk_file.open_smali('com/miui/securityscan/ui/main/MainContentFrame.smali')
-    specifier.name = 'onClick'
-    smali_file.method_nop(specifier)
-
-    smali_file = apk_file.open_smali('com/miui/securityscan/scanner/ScoreManager.smali')
-    specifier.name = None
-    specifier.keywords.append('getMinusPredictScore')
-    smali_file.method_return0(specifier)
-
-
-def disable_cloud_control(apk_file: ApkFile):
-    smali_file = apk_file.open_smali('com/miui/powerkeeper/cloudcontrol/LocalUpdateUtils.smali')
-    specifier = MethodSpecifier()
-    specifier.name = 'startCloudSyncData'
-    smali_file.method_nop(specifier)
-
-
-def global_maximum_fps(apk_file: ApkFile):
-    smali_file = apk_file.open_smali('com/miui/powerkeeper/statemachine/DisplayFrameSetting.smali')
-    specifier = MethodSpecifier()
-    specifier.name = 'setScreenEffect'
-    specifier.parameters = 'Ljava/lang/String;II'
-    smali_file.method_nop(specifier)
-
-
-@process_in_tmp
-def process_security_center():
-    print('>>> Process SecurityCenter.apk')
-    AdbUtils.pull('/system/priv-app/SecurityCenter/SecurityCenter.apk')
-    apk_file = ApkFile('SecurityCenter.apk')
-    apk_file.decode()
-
-    disable_wakeup_dialog(apk_file)
-    disable_wifi_blocked_notification(apk_file)
-    lock_100_score(apk_file)
-
-    path = apk_file.build()
-    AdbUtils.push_as_root(path, '/system/priv-app/SecurityCenter/')
-
-
-@process_in_tmp
-def process_power_keeper():
-    print('>>> Process PowerKeeper.apk')
-    AdbUtils.pull('/system/app/PowerKeeper/PowerKeeper.apk')
-    apk_file = ApkFile('PowerKeeper.apk')
-    apk_file.decode()
-
-    disable_cloud_control(apk_file)
-    global_maximum_fps(apk_file)
-
-    path = apk_file.build()
-    AdbUtils.push_as_root(path, '/system/app/PowerKeeper/')
-    AdbUtils.exec_as_root('rm -rf /data/vendor/thermal/config')
-
-
-@process_in_tmp
-def process_joyose():
-    print('>>> Process Joyose.apk')
-    AdbUtils.pull('/system/app/Joyose/Joyose.apk')
-    apk_file = ApkFile('Joyose.apk')
-    apk_file.decode()
-
-    smali_file = apk_file.find_smali('allow connect:')
-    specifier = MethodSpecifier()
-    specifier.keywords.append('allow connect:')
-    smali_file.method_nop(specifier)
-
-    path = apk_file.build()
-    AdbUtils.push_as_root(path, '/system/app/Joyose/')
-    AdbUtils.exec_as_root('pm clear com.xiaomi.joyose')
-
-
-@process_in_tmp
-def process_systemui():
-    print('>>> Process MiuiSystemUI.apk')
-    AdbUtils.pull('/system_ext/priv-app/MiuiSystemUI/MiuiSystemUI.apk')
-    apk_file = ApkFile('MiuiSystemUI.apk')
-    apk_file.decode()
-
-    smali_file = apk_file.open_smali('com/android/settingslib/bluetooth/LocalBluetoothAdapter.smali')
-    specifier = MethodSpecifier()
-    specifier.name = 'isSupportBluetoothRestrict'
-    smali_file.method_return0(specifier)
-
-    path = apk_file.build()
-    AdbUtils.push_as_root(path, '/system_ext/priv-app/MiuiSystemUI/')
 
 
 def unzip():
@@ -281,7 +142,7 @@ def compress_zip():
     flash_script_dir = 'META-INF/com/google/android'
     if not os.path.exists(flash_script_dir):
         os.makedirs(flash_script_dir)
-    shutil.copy('update-binary', os.path.join(flash_script_dir, 'update-binary'))
+    shutil.move('update-binary', os.path.join(flash_script_dir, 'update-binary'))
     shutil.copy(os.path.join(OVERLAY_DIR, 'zstd'), os.path.join(flash_script_dir, 'zstd'))
 
     _7z = os.path.join(BIN_DIR, '7za.exe')
