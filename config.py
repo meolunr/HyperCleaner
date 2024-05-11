800a93a5031db997b4fbe8afc493624fecf94836
meolunr <meolunr@gmail.com>
2024-05-11 19:30:54 +0800

    Update unpack partitions

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 SUPER_SIZE = 9126805504
 SUPER_PARTITIONS = ('mi_ext', 'odm', 'product', 'system', 'system_dlkm', 'system_ext', 'vendor', 'vendor_dlkm')
-UNPACK_PARTITIONS = ('mi_ext', 'odm', 'product', 'system', 'system_dlkm', 'system_ext', 'vendor', 'vendor_dlkm')
+UNPACK_PARTITIONS = ('product', 'system', 'system_ext', 'vendor')
 
 device = 'shennong'
 version = 'OS1.0.39.0'
