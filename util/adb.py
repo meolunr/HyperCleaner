8ed803de79e7f864f5130ae6a8674b7174cbbe86
meolunr <meolunr@gmail.com>
2024-05-13 01:50:05 +0800

    Add remove file function for test

--- util/adb.py
+++ util/adb.py
@@ -2,7 +2,8 @@ import os
 
 from hcglobal import OVERLAY_DIR
 
-_DATA_TMP = '/data/local/tmp/'
+_DATA_TMP_DIR = '/data/local/tmp/'
+_MODULE_DIR = '/data/adb/modules/hypercleaner'
 
 
 def execute(command: str):
@@ -10,13 +11,19 @@ def execute(command: str):
 
 
 def push(src: str, dst: str):
-    tmp_file = f'{_DATA_TMP}/{os.path.basename(src)}'
-    os.system(f'adb push {src} {_DATA_TMP}')
+    tmp_file = f'{_DATA_TMP_DIR}/{os.path.basename(src)}'
+    os.system(f'adb push {src} {_DATA_TMP_DIR}')
+    # Use cp and rm commands to avoid moving file permissions simultaneously
     execute(f'cp -rf {tmp_file} {dst}')
     execute(f'rm -rf {tmp_file}')
 
 
 def install_test_module():
-    module_dir = '/data/adb/modules/hypercleaner'
-    execute(f'mkdir -p {module_dir}')
-    push(f'{OVERLAY_DIR}/module.prop', module_dir)
+    execute(f'mkdir -p {_MODULE_DIR}')
+    push(f'{OVERLAY_DIR}/module.prop', _MODULE_DIR)
+
+
+def rm(file: str):
+    dir_name = os.path.dirname(file)
+    execute(f'mkdir -p {_MODULE_DIR}{dir_name}')
+    execute(f'mknod {_MODULE_DIR}{file} c 0 0')
