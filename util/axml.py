4169caeec8dd539d8d7abc58d5de0716e518acb6
meolunr <meolunr@gmail.com>
2024-06-11 13:15:07 +0800

    axml: Get string from the string pool

--- util/axml.py
+++ util/axml.py
@@ -34,3 +34,22 @@ class ManifestXml:
             f.seek(Chunk.CHUNK_HEADER_SIZE)
             self.string_chunk = StringChunk(f)
             self._string_cache = {}
+
+    def _get_string(self, idx: int):
+        if idx in self._string_cache:
+            return self._string_cache[idx]
+
+        offset = self.string_chunk.string_offsets[idx]
+        chars_start = offset + 2
+        if self.string_chunk.is_utf8:
+            struct_size = 1  # Skip byte length
+            format_string = '<B'
+            encoding = 'utf-8'
+        else:
+            struct_size = 2
+            format_string = '<H'
+            encoding = 'utf-16'
+        char_num = struct.unpack(format_string, self.string_chunk.string_pool[offset: offset + struct_size])[0]
+        chars = self.string_chunk.string_pool[chars_start: chars_start + char_num * struct_size].decode(encoding)
+        self._string_cache[idx] = chars
+        return chars
